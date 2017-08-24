ae370185e4d4bb128798661d7c3a5ec499403bfc
Yamila Moreno <yamila.ms@gmail.com>
2017-08-24 23:17:29 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+    // choose map providers between: https://leaflet-extras.github.io/leaflet-providers/preview/
+    var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFtaWxhIiwiYSI6IjUzNDE5ZDRkZjBiZjBiZDY0YTBhZjBmNmUyZGYzYTZiIn0.okLJEzGsBQ6IOgn1mhToIQ', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.outdoors'
+    });
+
+    var centerMap = [40.6390, -3.1229];
+
+    map = L.map('map', {
+        center: centerMap,
+        zoom: 9,
+        layers: [outdoors]
+    });
+
+    var hash = new L.Hash(map);
+
+    routes_bboxes = {};
+    $.get( "http://localhost:8000/routes", function( routes_dict ) {
+        for (var id in routes_dict) {
+            var customLink = "";
+            if (routes_dict[id].link){
+                customLink = "<br/><a href='" + routes_dict[id].link + "' target='new'>Ver historia</a>";
+            }
+            customIcon = categories[routes_dict[id].category].icon;
+            customColor = categories[routes_dict[id].category].color;
+
+            // Route gpx
+            gpx = new L.GPX(routes_dict[id].source, {
+                max_point_interval: 7200000,
+                gpx_options: {
+                    parseElements: ['route', 'track']
+                },
+                async: true,
+                marker_options: {
+                    startIcon: new L.AwesomeMarkers.icon({
+                        icon: customIcon,
+                        prefix: 'ion',
+                        markerColor: customColor,
+                        iconColor: 'white',
+                    }),
+                    startIconUrl: null,
+                    endIconUrl: null,
+                    shadowUrl: null
+                },
+                polyline_options: {
+                    color: customColor
+                },
+                customLink: customLink,
+                customIcon: customIcon
+            })
+            .on('loaded', function(e){
+                // Popup
+                var link = e.target.options.customLink;
+                var icon = e.target.options.customIcon;
+                var name = e.target.get_name();
+                var distance = (e.target.get_distance() / 1000).toFixed(2);
+                var content = "<i class='icon ion-" + icon + "'></i> <strong>" + name + "</strong> (" + distance + " km)" + link;
+                e.target.bindPopup(content);
+            });
+            routes_bboxes[id] = gpx;
+        }
+    });
+
+    map.on('moveend', function() {
+        // capturar NW y SE y seleccionar qué rutas voy a mostrar
+        console.log("NW: " + map.getBounds().getNorthWest() + " SE: " +  map.getBounds().getSouthEast());
+        var bboxMap = map.getBounds();
+        for (var id in routes_bboxes) {
+            if (isInside(routes_bboxes[id].getBounds(), bboxMap)) {
+                routes_bboxes[id].addTo(map);
+            }
+        }
+    });
+});
+
+function isInside(bboxRoute, bboxMap) {
+    return true;
+}
---
+++ js/routes.js
@@ -0,0 +1,18 @@
+var categories = {
+    "hiking": {
+        "color": "orange",
+        "icon": "android-walk"
+    },
+    "biking": {
+        "color": "green",
+        "icon": "android-bicycle"
+    },
+    "trip": {
+        "color": "blue",
+        "icon": "android-walk"
+    },
+    "car": {
+        "color": "red",
+        "icon": "android-car"
+    }
+};
